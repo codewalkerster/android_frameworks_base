fd26127d86688733c18d902bc08475d225ca39e4
Luke Go <luke.go@hardkernel.com>
2019-04-22 17:41:51 +0900

    ODROID: Added shortcut functions.
    Change-Id: Id683329774b200c6d7584979218b87ececa80467
    Signed-off-by: Luke go <sangch.go@gmail.com>

--- core/java/android/view/WindowManagerImpl.java
+++ core/java/android/view/WindowManagerImpl.java
@@ -28,6 +28,7 @@ import android.annotation.UiContext;
 import android.compat.annotation.UnsupportedAppUsage;
 import android.content.ComponentName;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Region;
 import android.os.Bundle;
@@ -466,5 +467,25 @@ public final class WindowManagerImpl implements WindowManager {
         } catch (RemoteException e) {
             throw e.rethrowFromSystemServer();
         }
+    }
+
+    @Override
+    public void setApplicationShortcut(int keyCode, Intent intent) {
+        try {
+            WindowManagerGlobal.getWindowManagerService()
+                .setApplicationShortcut(keyCode, intent);
+        } catch (RemoteException e) {
+        }
+    }
+
+    @Override
+    public String getApplicationOfShortcutAt(int keyCode) {
+        String result = null;
+        try {
+            WindowManagerGlobal.getWindowManagerService()
+                .getApplicationOfShortcutAt(keyCode);
+        } catch (RemoteException e) {
+        }
+        return result;
     }
 }
