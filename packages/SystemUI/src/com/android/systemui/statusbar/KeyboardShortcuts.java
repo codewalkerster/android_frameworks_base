f5a8b65e5003aeea419b07f4d5d98f45818910e4
Ben Murdoch <benm@google.com>
2024-08-30 17:22:59 +0000

    RESTRICT AUTOMERGE Clear app-provided shortcut icons
    When displaying keyboard shortcuts provided by an app, clear
    any icon that may have been set (this is only possible via
    reflection, and is not a intended for usage outside of the system).
    
    Bug: 331180422
    Test: Verify on device
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:8dc5ed150f2c0e08870f656f3170f304ba9ad260)
    Merged-In: If7e291eb2254c3cbec23673c65e7477e6ad45b09
    Change-Id: If7e291eb2254c3cbec23673c65e7477e6ad45b09

--- packages/SystemUI/src/com/android/systemui/statusbar/KeyboardShortcuts.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/KeyboardShortcuts.java
@@ -413,6 +413,7 @@ public final class KeyboardShortcuts {
         mReceivedAppShortcutGroups = null;
         mReceivedImeShortcutGroups = null;
 
+	 sanitiseShortcuts(shortcutGroups);
         final KeyboardShortcutGroup defaultAppShortcuts =
                 getDefaultApplicationShortcuts();
         if (defaultAppShortcuts != null) {
@@ -420,6 +421,14 @@ public final class KeyboardShortcuts {
         }
         shortcutGroups.add(getSystemShortcuts());
         showKeyboardShortcutsDialog(shortcutGroups);
+    }
+
+    static void sanitiseShortcuts(List<KeyboardShortcutGroup> shortcutGroups) {
+        for (KeyboardShortcutGroup group : shortcutGroups) {
+            for (KeyboardShortcutInfo info : group.getItems()) {
+                info.clearIcon();
+            }
+        }
     }
 
     private void dismissKeyboardShortcuts() {
