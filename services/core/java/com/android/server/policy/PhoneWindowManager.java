83f34cc1c439b021ba7c3e20a4c5cecac3e44872
Vaibhav Devmurari <vdevmurari@google.com>
2024-12-09 13:15:03 +0000

    Fix: Block opening settings app on keyguard without user auth
    Test: atest KeyGestureEventTests
    Bug: 378900798
    Flag: EXEMPT bugfix
    (cherry picked from commit d615298466085c4a88c6733804160e0c1ee7e31e)
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:e4d483a2ef99a71c6fcd6ad2e6c2f8f88ba380f4)
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:e832d2b23784ede62259fb7518573e12dced2209)
    Merged-In: I89d43872108710d20e0c4ef7e652d389896155d7
    Change-Id: I89d43872108710d20e0c4ef7e652d389896155d7

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -3201,7 +3201,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 }
                 break;
             case KeyEvent.KEYCODE_I:
-                if (firstDown && event.isMetaPressed()) {
+                if (down && event.isMetaPressed() && isUserSetupComplete() && !keyguardOn) {
                     showSystemSettings();
                     logKeyboardSystemsEvent(event, KeyboardLogEvent.LAUNCH_SYSTEM_SETTINGS);
                     return true;
